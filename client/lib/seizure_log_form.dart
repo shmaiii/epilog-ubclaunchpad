4cbef625ac20750068c6e94086344a6376920c7d
TylerQube <tylersmith1308@gmail.com>
2022-11-18 22:44:55 -0800

    Add prototype for new seizure log form page.

---
+++ client/lib/seizure_log_form.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class SeizureLogPage extends StatelessWidget {
+  const SeizureLogPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Scaffold(
+          appBar: AppBar(),
+          body: const SeizureLogForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class SeizureLogForm extends StatefulWidget {
+  const SeizureLogForm({super.key});
+
+  @override
+  State<SeizureLogForm> createState() => _SeizureLogFormState();
+}
+
+class _SeizureLogFormState extends State<SeizureLogForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 40.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'New Seizure Log',
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 35.0),
+                  ),
+                  const _FormTextInput(
+                    label: "What would you like to name this entry?",
+                    hintText: "New Title",
+                  ),
+                  const _FormTextInput(
+                    label: "When did the seizure happen?",
+                    hintText: "Date and Time",
+                  ),
+                  const _FormTextInput(
+                    label: "How long did the seizure last?",
+                    hintText: "Duration",
+                  ),
+                  const _FormTextInput(
+                    label: "What were you doing at the time of the seizure?",
+                    hintText: "Activity",
+                  ),
+                  Stack(
+                      alignment: Alignment.bottomRight,
+                      clipBehavior: Clip.none,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // Validate will return true if the form is valid, or false if
+                            // the form is invalid.
+                            if (_formKey.currentState!.validate()) {
+                              // Process data.
+                            }
+                          },
+                          child: const Text('Submit'),
+                        ),
+                      ])
+                ])));
+  }
+}
+
+@immutable
+class _FormTextInput extends StatelessWidget {
+  const _FormTextInput({
+    required this.label,
+    required this.hintText,
+  });
+
+  final String label;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            label,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: hintText,
+                enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 4.0)),
+                focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 4.0))),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          )
+        ]));
+  }
+}
