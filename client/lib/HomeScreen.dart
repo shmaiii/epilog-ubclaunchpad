2da224d3be6060a876b7f4f3ea79a4aefe74fea6
29705 <luyang.jiang2002@outlook.com>
2022-11-18 20:12:00 -0800

    First Draft of navigation bar and pages setup

---
+++ client/lib/HomeScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeState();
+}
+
+class _HomeState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Home', style: demotextstyle,),
+      ),
+    );
+  }
+}
---
+++ client/lib/ProfileScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Profile', style: demotextstyle,),
+      ),
+    );
+  }
+}
