f6b5c163a1ef2119f68a2b88a18e3a945368bbde
TylerQube <tylersmith1308@gmail.com>
2022-12-02 01:43:46 -0800

    Add multi-page form architecture, render from array.

--- client/lib/seizure_log_form.dart
+++ client/lib/seizure_log_form.dart
@@ -1,6 +1,8 @@
-import 'package:client/duration_picker.dart';
+import 'package:client/EntryFormPages/CategoryType.dart';
+import 'package:client/EntryFormPages/Symptoms.dart';
 import 'package:flutter/material.dart';
-import 'date_time_input.dart';
+import 'EntryFormPages/NewEntry.dart';
+import 'dart:math';
 
 class SeizureLogPage extends StatelessWidget {
   const SeizureLogPage({Key? key, this.title}) : super(key: key);
@@ -25,123 +27,103 @@ class SeizureLogForm extends StatefulWidget {
   State<SeizureLogForm> createState() => _SeizureLogFormState();
 }
 
+List<GlobalKey<FormState>> formKeys =
+    List<GlobalKey<FormState>>.generate(pageTitles.length, (i) {
+  return GlobalKey();
+});
+
+List<Widget> formPages = [
+  NewEntry(formKey: formKeys[0]),
+  CategoryType(formKey: formKeys[1]),
+  Symptoms(formKey: formKeys[2]),
+];
+
+List<String> pageTitles = [
+  "New Seizure Log",
+  "Category and Type",
+  "Symptoms",
+];
+
 class _SeizureLogFormState extends State<SeizureLogForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  int formPageInd = 0;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
-      child: Column(children: [
-        const Padding(
-          padding: EdgeInsets.only(top: 30, left: 30),
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: BackButton(),
-          ),
+        child: Column(children: [
+      const Padding(
+        padding: EdgeInsets.only(top: 30, left: 30),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: BackButton(),
         ),
-        Column(children: [
-          Form(
-            key: _formKey,
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 40.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Text(
-                    'New Seizure Log',
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 35.0),
-                  ),
-                  const _FormTextInput(
-                    label: "What would you like to name this entry?",
-                    hintText: "New Title",
-                  ),
-                  DateTimeInput(
-                    label: "When did this seizure occur?",
-                  ),
-                  const DurationPicker(
-                    label: "How long did the seizure last?",
-                  ),
-                  const _FormTextInput(
-                    label: "What were you doing at the time of the seizure?",
-                    hintText: "Activity",
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ]),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 30),
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Validate will return true if the form is valid, or false if
-                  // the form is invalid.
-                  if (_formKey.currentState!.validate()) {
-                    // Process data.
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(100, 60),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                ),
-                child: const Text('Next >',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
-            ),
-          ),
-        ),
+      ),
+      Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+        Text(
+          pageTitles[formPageInd],
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 35.0),
+        )
       ]),
-    );
-  }
-}
-
-@immutable
-class _FormTextInput extends StatelessWidget {
-  const _FormTextInput({
-    required this.label,
-    required this.hintText,
-  });
-
-  final String label;
-  final String hintText;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Text(
-            label,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
-          ),
-          TextFormField(
-            maxLength: 50,
-            decoration: InputDecoration(
-                hintText: hintText,
-                helperText: " ",
-                enabledBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 4.0)),
-                focusedBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 4.0))),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          )
-        ]));
+      formPages[formPageInd],
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+          child: Row(children: [
+            if (formPageInd > 0)
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      formPageInd = max(formPageInd - 1, 0);
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(100, 60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                  child: const Text('< Back',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ),
+              ),
+            const Spacer(),
+            if (formPageInd < formPages.length - 1)
+              Align(
+                alignment: Alignment.bottomRight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate will return true if the form is valid, or false if
+                    // the form is invalid.
+                    if (formKeys[formPageInd].currentState!.validate()) {
+                      setState(() {
+                        formPageInd =
+                            min(formPageInd + 1, formPages.length - 1);
+                      });
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(100, 60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                  child: const Text('Next >',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ),
+              ),
+          ]),
+        ),
+      ),
+    ]));
   }
 }
