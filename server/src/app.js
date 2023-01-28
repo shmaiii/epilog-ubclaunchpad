789f66cb407f53f2eda1cf9816309e0d0c4e9e16
shmaiii <luongxuanmai2109@gmail.com>
2023-01-28 00:24:44 -0800

    set up backend for mux testing

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,12 @@
+require('dotenv').config();
 import express from 'express';
 import cors from 'cors';
+
+const Mux = require("@mux/mux-node");
+const { Video } = new Mux(
+    process.env.MUX_TOKEN_ID,
+    process.env.MUX_SECRET_KEY
+);
 
 const app = express();
 
@@ -14,4 +21,58 @@ app.listen(PORT, () => {
 app.get('/ping', (req, res) => {
     console.log("pong");
     res.status(200).send("pong");
+});
+
+// API routes for video processing
+app.post("/assets", async(req, res) => {
+    console.log("body: " + req.body.videoUrl);
+
+    const asset = await Video.Assets.create({
+        input: req.body.videoUrl,
+        playback_policy: "public",
+    });
+
+    res.json({
+        data: {
+            id: asset.id,
+            status: asset.status,
+            playback_ids: asset.playback_ids,
+            created_at: asset.created_at,
+        },
+    });
+});
+
+app.get("/assets", async(req, res) => {
+    const assets = await Video.Assets.list();
+
+    res.json({
+        data: assets.map((asset) => ({
+            id: asset.id,
+            status: asset.status,
+            playback_ids: asset.created_at,
+            duration: asset.duration,
+            max_stored_resolution: asset.max_stored_resolution,
+            max_stored_frame_rate: asset.max_stored_frame_rate,
+            aspect_ratio: asset.aspect_ratio,
+        })),
+    });
+});
+
+app.get("/asset", async (req, res) => {
+    let videoId = req.query.videoId;
+    const asset = await Video.Assets.get(videoId);
+
+    console.log(asset);
+
+    res.json({
+        data: {
+            id: asset.id,
+            status: asset.status,
+            playback_ids: asset.created_at,
+            duration: asset.duration,
+            max_stored_resolution: asset.max_stored_resolution,
+            max_stored_frame_rate: asset.max_stored_frame_rate,
+            aspect_ratio: asset.aspect_ratio,
+        },
+    });
 });
