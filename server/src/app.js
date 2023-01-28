3cb7a8ffb18477607651b3936df11d1a915158a9
SimonX13 <simonxia13@icloud.com>
2023-01-28 02:26:08 -0800

    backend

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import { addDoc, collection, doc, getDoc, getFirestore, setDoc, updateDoc } from "firebase/firestore";
 import { db } from './firebase/db.js';
 
+
+
 const app = express();
 
 app.use(express.json());
@@ -33,7 +35,42 @@ app.get('/user/:id/personal-information/read', async (req, res) => {
     try {
         const snapshot = await getDoc(testSpecial);
         if (snapshot.exists()) {
-            const docData = snapshot.data();
+            const docData = snapshot.data().doctors;
+            console.log(docData)
+            res.status(200).send(`Data is ${JSON.stringify(docData)}`);
+        }
+    } catch (error) {
+        console.log("Got an error");
+        console.log(error);
+    }
+
+});
+
+app.get('/user/:id/patientsdoctors/read', async (req, res) => {
+    console.log("getting user's personal info from db");
+
+    const id = req.params.id;
+    const testSpecial = doc(db, "/users/" + id);
+    var docData;
+    try {
+        const snapshot = await getDoc(testSpecial);
+        if (snapshot.exists()) {
+            docData = snapshot.data().doctors;
+            //console.log(docData)
+            //res.status(200).send(`Data is ${JSON.stringify(docData)}`);
+        }
+    } catch (error) {
+        console.log("Got an error");
+        console.log(error);
+    }
+    //console.log(docData)
+    const xx = doc(db, "/contacts/" + docData[0] );
+    
+    try {
+        const snapshot = await getDoc(xx);
+        if (snapshot.exists()) {
+            docData = snapshot.data();
+            console.log(docData)
             res.status(200).send(`Data is ${JSON.stringify(docData)}`);
         }
     } catch (error) {
@@ -62,14 +99,82 @@ app.post('/medications/:mid/edit', async (req, res) => {
 
 // CONTACTS ENDPOINTS
 
+const readIds = async (collection, ids)=>{
+    const reads = ids.map(id => collection.doc(id).get())
+    const result = await Promise.all(reads)
+    return result.map(v => v.data())
+}
+
+
 app.get('/user/:id/contacts/read', async (req, res) => {
     console.log("getting user's contacts from db");
+   
+
+    const id = req.params.id;
+    const testSpecial = doc(db, "/users/" + id);
+    var docDataArray;
+    try {
+        const snapshot = await getDoc(testSpecial);
+        if (snapshot.exists()) {
+            docDataArray = snapshot.data().Doctors;
+            console.log(docDataArray)
+        }
+    } catch (error) {
+        console.log("Got an error");
+        console.log(error);
+    }
+    let result = {};
+    try {
+        for(var i =0; i< docDataArray.length; i++){
+            const snapshot = await getDoc(docDataArray[i]);
+            if (snapshot.exists()) {
+                const docData = snapshot.data();
+                //console.log(docData)
+                result[i] = docData;
+                
+            }
+        }
+        console.log(result)
+        res.status(200).send(`Data is ${JSON.stringify(result)}`);
+       
+    } catch (error) {
+        console.log("Got an error");
+        console.log(error);
+    }
 });
 
+/**
+ * still need to figure out how to assign a auto-generated id to each document 
+ */
 app.post('/contacts/add', async (req, res) => {
     console.log("adding user's contact in db");
+    const data = {
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+    }
+
+    await setDoc(doc(db, "/users/", "1"), data);
+    //res.status(200).send(`Data is ${JSON.stringify(result)}`);
 });
 
+
+// does not work
 app.post('/contacts/:cid/edit', async (req, res) => {
-    console.log("editing user's contact in db");
+    const id = req.params.cid;
+    const reqBody = {
+        first : req.body.fullName,
+        last : req.body.height,
+        born: 999
+    }
+    console.log(req)
+    console.log("adding user's contact in db");
+    
+    const data = {
+        first: 'aha',
+        last: 'changed',
+        born: 2002
+    }
+
+    await updateDoc(doc(db, "/users/", id), reqBody);
 });
