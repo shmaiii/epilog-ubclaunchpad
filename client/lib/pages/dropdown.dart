7e572b2e1c1b7726f445454c7d612d50df5a8f84
TylerQube <tylersmith1308@gmail.com>
2023-02-03 16:18:49 -0800

    Use container decoration for dropdown underline.

--- client/lib/pages/dropdown.dart
+++ client/lib/pages/dropdown.dart
@@ -23,31 +23,34 @@ class _DropdownButtonExampleState extends State<DropdownButtonExample> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue ?? "",
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      isExpanded: true,
-      style: const TextStyle(
-        color: Colors.black,
-        fontSize: 17,
-      ),
-      underline: Container(
-        height: 4,
-        color: Colors.black,
-      ),
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      items: widget.list.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
+    return Container(
+        decoration: const BoxDecoration(
+            border: Border(
+                bottom: BorderSide(
+          color: Colors.black,
+          width: 4.0, // This would be the width of the underline
+        ))),
+        child: DropdownButton<String>(
+          value: dropdownValue ?? "",
+          icon: const Icon(Icons.arrow_downward),
+          elevation: 16,
+          isExpanded: true,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 17,
+          ),
+          onChanged: (String? value) {
+            // This is called when the user selects an item.
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: widget.list.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ));
   }
 }
