1cf2132427a9ea18c2814b85a64ecf62c98134e0
James T Ardian <jamesardian01@gmail.com>
2023-02-04 12:07:58 -0800

    remove unnecessary backend

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import calendarRoutes from './routes/calendarRoutes.js';
 
-import { addDoc, collection, doc, getDoc, getFirestore, setDoc, updateDoc, getDocs } from "firebase/firestore";
-import { db } from './firebase/db.js';
-
 const app = express();
 
 app.use(express.json());
@@ -15,51 +12,6 @@ app.use('/calendar', calendarRoutes);
 app.get('/ping', (req, res) => {
     console.log("pong");
     res.status(200).send("pong");
-});
-
-// class Reminder {
-//     constructor(type, title, date, time, notes) {
-//         this.type = type;
-//         this.title = title;
-//         this.date = date;
-//         this.time = time;
-//         this.notes = notes;
-//     }
-// }
-
-// Endpoint for Get request for user's reminders
-app.get('/Users/:id/Reminders', async (req, res) => {
-    console.log("Get user's reminders");
-
-    const id = req.params.id;
-    var reminders = [];
-
-    try {
-        const reminderSnapshot = await getDocs(collection(db, "/users/" + id + "/Reminders"));
-
-        // Add all reminder objects into an array
-        reminderSnapshot.docs.forEach((doc) => {
-            reminders.push(doc.data());
-        });
-
-        // Send the array of reminders back to client
-        res.status(200).send(JSON.stringify(reminders));
-    } catch (error) {
-        console.log("error while getting user reminders");
-        console.log(error);
-    }
-
-});
-
-// Post request endpoint for adding new reminder
-app.post('/new_reminder', async (req, res) => {
-    console.log("entering new reminder post request end point");
-
-    const id = req.body.id;
-    const new_reminder = req.body.reminder;
-    const docRef = await addDoc(collection(db, "/users/" + id + "/Reminders"), new_reminder)
-
-    res.status(200).send();
 });
 
 app.use((error, req, res, next) => {
