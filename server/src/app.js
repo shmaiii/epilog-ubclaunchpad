b022d1a5f8b099755841650e49f47781dd839d32
James T Ardian <jamesardian01@gmail.com>
2023-02-24 19:32:28 -0800

    err object from firebase is sometimes immutable so deleted any mutation to err object

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,7 @@ app.get('/ping', (req, res) => {
 
 app.use((error, req, res, next) => {
     console.log(error)
-    return res.status(error.code ?? 400).json({err: error.message})
+    return res.status(error.code ?? 500).json({err: error.message})
 });
 
 const PORT = 8080;
