fa76ca9fc2cd36cea0e8517bd2acfcf66ea72858
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2023-03-25 12:27:57 -0700

    fixed import issue

--- client/lib/screens/RecordScreen.dart
+++ client/lib/screens/RecordScreen.dart
@@ -2,20 +2,19 @@ import '../main.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
-import 'dart:io' as io; 
+import 'dart:io' as io;
 import 'dart:typed_data';
 import 'package:camera/camera.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:uri_to_file/uri_to_file.dart';
-import 'package:content_resolver/content_resolver.dart';
 import 'VideoPlayback.dart';
 
 class RecordingPage extends StatelessWidget {
-  const RecordingPage({Key? key}) : super(key:key);
+  const RecordingPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: RecordScreen(),
@@ -24,43 +23,45 @@ class RecordingPage extends StatelessWidget {
 }
 
 class RecordScreen extends StatefulWidget {
-  const RecordScreen({Key? key}): super(key: key);
+  const RecordScreen({Key? key}) : super(key: key);
 
   @override
   State<RecordScreen> createState() => _RecordScreenState();
 }
 
 class _RecordScreenState extends State<RecordScreen> {
-
   late CameraController _cameraController;
   bool _isLoading = true;
   bool _isRecording = false;
-  late List <CameraDescription> _availableCameras;
+  late List<CameraDescription> _availableCameras;
 
   _getAvailableCameras() async {
     _availableCameras = await availableCameras();
-    _initCamera(_availableCameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.front));  
-    }
+    _initCamera(_availableCameras.firstWhere(
+        (camera) => camera.lensDirection == CameraLensDirection.front));
+  }
 
   _initCamera(CameraDescription description) async {
-    
-    _cameraController = CameraController(description, ResolutionPreset.max, enableAudio: true);
+    _cameraController =
+        CameraController(description, ResolutionPreset.max, enableAudio: true);
     try {
       await _cameraController.initialize();
       setState(() {
         _isLoading = false;
       });
-    } catch (e) { print(e); }
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    _getAvailableCameras();  
+    _getAvailableCameras();
   }
 
   @override
-  void dispose(){
+  void dispose() {
     _cameraController.dispose();
     super.dispose();
   }
@@ -72,9 +73,9 @@ class _RecordScreenState extends State<RecordScreen> {
         _isRecording = false;
       });
       final route = MaterialPageRoute(
-      fullscreenDialog: true,
-      builder: (_) => VideoPage(filePath: file.path),
-    );
+        fullscreenDialog: true,
+        builder: (_) => VideoPage(filePath: file.path),
+      );
       Navigator.push(context, route);
     } else {
       await _cameraController.prepareForVideoRecording();
@@ -89,18 +90,20 @@ class _RecordScreenState extends State<RecordScreen> {
     final lensDirection = _cameraController.description.lensDirection;
     CameraDescription newDescription;
     if (lensDirection == CameraLensDirection.front) {
-     newDescription = _availableCameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.back);
+      newDescription = _availableCameras.firstWhere(
+          (camera) => camera.lensDirection == CameraLensDirection.back);
     } else {
-      newDescription = _availableCameras.firstWhere((element) => element.lensDirection == CameraLensDirection.front);
+      newDescription = _availableCameras.firstWhere(
+          (element) => element.lensDirection == CameraLensDirection.front);
     }
 
-    if (newDescription != null){
+    if (newDescription != null) {
       _initCamera(newDescription);
     } else {
       print('newDescription is null');
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     if (_isLoading) {
@@ -125,30 +128,28 @@ class _RecordScreenState extends State<RecordScreen> {
               ),
             ),
             Align(
-              alignment: Alignment.bottomRight,
-              child: FloatingActionButton(
-                child: Icon(Icons.switch_camera),
-                onPressed: () => _switchCamera(),
-              )
-            )
+                alignment: Alignment.bottomRight,
+                child: FloatingActionButton(
+                  child: Icon(Icons.switch_camera),
+                  onPressed: () => _switchCamera(),
+                ))
           ],
         ),
       );
-     }
+    }
   }
 }
-
 
 //video testing page pop up right after recording
 class VideoPage extends StatefulWidget {
   final String filePath;
-  const VideoPage({Key? key, required this.filePath}): super(key: key);
+  const VideoPage({Key? key, required this.filePath}) : super(key: key);
 
   @override
   State<VideoPage> createState() => _VideoPageState();
 }
 
-class _VideoPageState extends  State<VideoPage> {
+class _VideoPageState extends State<VideoPage> {
   late VideoPlayerController _videoPlayerController;
   late Future<void> _initializeVideo;
 
@@ -161,17 +162,18 @@ class _VideoPageState extends  State<VideoPage> {
   //   _videoPlayerController.play();
   // }
 
-
-  @override 
-  void dispose(){
+  @override
+  void dispose() {
     _videoPlayerController.dispose();
     super.dispose();
   }
-    Future _initVideoPlayer() async {
-      _videoPlayerController = VideoPlayerController.file(io.File(widget.filePath));
-      await _videoPlayerController.initialize();
-      await _videoPlayerController.setLooping(true); // video will loop
-      await _videoPlayerController.play();
+
+  Future _initVideoPlayer() async {
+    _videoPlayerController =
+        VideoPlayerController.file(io.File(widget.filePath));
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true); // video will loop
+    await _videoPlayerController.play();
   }
 
   // Future<io.File> convertUriToFile(String uriString) async {
@@ -185,16 +187,16 @@ class _VideoPageState extends  State<VideoPage> {
   //   } catch (e) {
   //     print(e);
   //   }
-    
+
   //   return io.File('');
   // }
 
   void _saveVideo() async {
     print(widget.filePath);
     var result = await ImageGallerySaver.saveFile(widget.filePath);
-    print (result);
+    print(result);
     var path;
-    if (result == null){
+    if (result == null) {
       path = '';
     } else {
       path = result['filePath'];
@@ -204,9 +206,9 @@ class _VideoPageState extends  State<VideoPage> {
     //dispose(); // later changed to link to entry page or sth
 
     // content resolver take persistable permission ? working rn might need this if there's bug
-    
+
     // try{
-      io.File file = await toFile(path);
+    io.File file = await toFile(path);
     // } on UnsupportedError catch (e) {
     //   print (e.message);
     // } on io.IOException catch (e) {
@@ -219,33 +221,31 @@ class _VideoPageState extends  State<VideoPage> {
       fullscreenDialog: true,
       builder: (_) => VideoPlaybackPage(file: file),
     );
-      Navigator.push(context, route);
+    Navigator.push(context, route);
   }
 
-  @override 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.check),
-            onPressed: () {
-              print("icon pressed");
-              _saveVideo();
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.check),
+                onPressed: () {
+                  print("icon pressed");
+                  _saveVideo();
+                })
+          ],
+        ),
+        extendBodyBehindAppBar: true,
+        body: FutureBuilder(
+            future: _initVideoPlayer(),
+            builder: (context, state) {
+              if (state.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                return VideoPlayer(_videoPlayerController);
               }
-            )],
-      ),
-      extendBodyBehindAppBar: true,
-      body: FutureBuilder(
-        future: _initVideoPlayer(),
-        builder:(context, state) {
-          if (state.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            return VideoPlayer(_videoPlayerController);
-          }
-        }
-      )
-    );
+            }));
   }
 }
