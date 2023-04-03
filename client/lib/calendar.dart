54d2387f12c493d528039521eada63e299f90a78
TonyLiu0226 <TL0226yn@gmail.com>
2023-04-02 17:27:46 -0700

    adds calendar loading screen

--- client/lib/calendar.dart
+++ client/lib/calendar.dart
@@ -71,68 +71,81 @@ class _TableBasicsExampleState extends State<TableBasicsExample> {
               fontSize: 22,
             )),
       ),
-      body: Column(
-        children: [
-          TableCalendar<Event>(
-            firstDay: kFirstDay,
-            lastDay: kLastDay,
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
-            eventLoader: (day) => allEvents[day]!,
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            calendarStyle: const CalendarStyle(
-              outsideDaysVisible: false,
-            ),
-            onDaySelected: _onDaySelected,
-            onFormatChanged: (format) {
-              if (_calendarFormat != format) {
-                setState(() {
-                  _calendarFormat = format;
-                });
-              }
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
-            },
-          ),
-          const SizedBox(height: 8.0),
-          Expanded(
-            child: ValueListenableBuilder<List<Event>>(
-              valueListenable: _selectedEvents,
-              builder: (context, value, _) {
-                return ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 12.0,
-                        vertical: 4.0,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: ListTile(
-                          onTap: () => print(
-                              'yo'), //replace this line of code to open a modal that displays the info of the event
-                          title: Row(
-                            children: [
-                              Expanded(child: Text('${value[index].title}')),
-                              SizedBox(width: 50),
-                              Text(
-                                  '${value[index].date.toString().split(' ')[1].split(':')[0] + ':' + value[index].date.toString().split(' ')[1].split(':')[1]}')
-                            ],
-                          )),
-                    );
+      body: FutureBuilder<Map<DateTime, List<Event>>>(
+        future: getEvents(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            allEvents = snapshot.data!;
+            return Column(
+              children: [
+                TableCalendar<Event>(
+                  firstDay: kFirstDay,
+                  lastDay: kLastDay,
+                  focusedDay: _focusedDay,
+                  calendarFormat: _calendarFormat,
+                  selectedDayPredicate: (day) {
+                    return isSameDay(_selectedDay, day);
                   },
-                );
-              },
-            ),
-          ),
-        ],
+                  eventLoader: (day) => allEvents[day]!,
+                  startingDayOfWeek: StartingDayOfWeek.monday,
+                  calendarStyle: const CalendarStyle(
+                    outsideDaysVisible: false,
+                  ),
+                  onDaySelected: _onDaySelected,
+                  onFormatChanged: (format) {
+                    if (_calendarFormat != format) {
+                      setState(() {
+                        _calendarFormat = format;
+                      });
+                    }
+                  },
+                  onPageChanged: (focusedDay) {
+                    _focusedDay = focusedDay;
+                  },
+                ),
+                const SizedBox(height: 8.0),
+                Expanded(
+                  child: ValueListenableBuilder<List<Event>>(
+                    valueListenable: _selectedEvents,
+                    builder: (context, value, _) {
+                      return ListView.builder(
+                        itemCount: value.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 12.0,
+                              vertical: 4.0,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            child: ListTile(
+                                onTap: () => print(
+                                    'yo'), //replace this line of code to open a modal that displays the info of the event
+                                title: Row(
+                                  children: [
+                                    Expanded(
+                                        child: Text('${value[index].title}')),
+                                    SizedBox(width: 50),
+                                    Text(
+                                        '${value[index].date.toString().split(' ')[1].split(':')[0] + ':' + value[index].date.toString().split(' ')[1].split(':')[1]}')
+                                  ],
+                                )),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          // By default, show a loading spinner
+          return Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
